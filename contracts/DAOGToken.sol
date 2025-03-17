098c6ce434a77dc4fa6605997ca758d99f70f66b
Ziqian-Zhang <zz3111@nyu.edu>
2025-03-16 22:20:24 -0400

    updated contracts

--- contracts/DAOGToken.sol
+++ contracts/DAOGToken.sol
@@ -1,48 +1,66 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
+
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
 
 contract DAOGToken is ERC20, Ownable {
-    mapping(address => uint256) public stakedTokens;
-    mapping(address => uint256) public stakingStart;
+   using Math for uint256;
 
-    constructor(address initialOwner)
-        ERC20("DAO Governance Token", "DAOG")
-        Ownable(initialOwner)
-    {
-        _mint(initialOwner, 1000000 * 10**18); // Mint 1 million tokens to owner
-    }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
+   mapping(address => uint256) public stakedTokens;
+   mapping(address => uint256) public stakingStart;
 
-    function stake(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, "Not enough tokens to stake");
-        _transfer(msg.sender, address(this), amount);
 
-        if (stakedTokens[msg.sender] == 0) {
-            stakingStart[msg.sender] = block.timestamp; // Track staking time
-        }
+   constructor(address initialOwner)
+       ERC20("DAO Governance Token", "DAOG")
+       Ownable(initialOwner)
+   {
+       _mint(initialOwner, 1000000 * 10**18); // Mint 1 million tokens to owner
+   }
 
-        stakedTokens[msg.sender] += amount;
-    }
 
-    function getVotingPower(address user) public view returns (uint256) {
-        uint256 timeStaked = block.timestamp - stakingStart[user];
-        uint256 weight = 1 + (timeStaked / 30 days); // 1x multiplier every 30 days
-        return sqrt(stakedTokens[user]) * weight;
-    }
+   function mint(address to, uint256 amount) public onlyOwner {
+       _mint(to, amount);
+   }
 
-    function sqrt(uint256 x) internal pure returns (uint256) {
-        uint256 z = (x + 1) / 2;
-        uint256 y = x;
-        while (z < y) {
-            y = z;
-            z = (x / z + z) / 2;
-        }
-        return y;
-    }
+
+   // Quadratic Voting Calculation
+   function getVotingPower(address user) public view returns (uint256) {
+       return Math.sqrt(stakedTokens[user]);
+   }
+
+
+   // Staking function
+   function stakeTokens(uint256 amount) external {
+       require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+       _transfer(msg.sender, address(this), amount);
+       stakedTokens[msg.sender] += amount;
+       stakingStart[msg.sender] = block.timestamp;
+   }
+
+
+   // Unstaking with vesting bonus
+   function unstakeTokens() external {
+       require(stakedTokens[msg.sender] > 0, "No staked tokens");
+
+
+       uint256 stakingDuration = block.timestamp - stakingStart[msg.sender];
+       uint256 bonus = calculateVestingBonus(stakedTokens[msg.sender], stakingDuration);
+
+
+       uint256 totalAmount = stakedTokens[msg.sender] + bonus;
+       stakedTokens[msg.sender] = 0;
+       _transfer(address(this), msg.sender, totalAmount);
+   }
+
+
+   // Vesting calculation for staking rewards
+   function calculateVestingBonus(uint256 amount, uint256 duration) internal pure returns (uint256) {
+       uint256 months = duration / 30 days;
+       return (amount * months) / 100; // 1% bonus per month
+   }
 }
--- contracts/TreasuryDAO.sol
+++ contracts/TreasuryDAO.sol
@@ -1,124 +1,161 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
+
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
-import "@aave/core-v3/contracts/interfaces/IPool.sol";
+import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract TreasuryDAO is Ownable, KeeperCompatibleInterface {
-    struct Proposal {
-        address payable recipient;
-        uint256 amount;
-        uint256 votes;
-        uint256 endTime;
-        uint256 unlockTime;  // Time-lock for fund withdrawal
-        bool executed;
-        mapping(address => bool) voted;
-    }
 
-    mapping(address => bool) public members;
-    Proposal[] public proposals;
-    IERC20 public governanceToken;
-    AggregatorV3Interface internal priceFeed;
-    IPool public lendingPool;
-    uint256 public minVoteDuration = 1 days;
-
-    event ProposalCreated(uint proposalId, address recipient, uint256 amount);
-    event Voted(uint proposalId, address voter, uint256 weight);
-    event ProposalExecuted(uint proposalId);
-
-    constructor(address tokenAddress, address oracle, address aavePool, address initialOwner) Ownable(initialOwner) {
-        governanceToken = IERC20(tokenAddress);
-        priceFeed = AggregatorV3Interface(oracle); // Chainlink ETH/USD Price Feed
-        lendingPool = IPool(aavePool);
-        members[initialOwner] = true;
-    }
-
-    modifier onlyMember() {
-        require(members[msg.sender], "Not a DAO member");
-        _;
-    }
-
-    function addMember(address member) public onlyOwner {
-        members[member] = true;
-    }
-
-    function proposeFunding(address payable recipient, uint256 amount) public onlyMember {
-        require(address(this).balance >= amount, "Insufficient treasury balance");
-        require(getETHPrice() > 1500, "ETH price too low");
-
-        proposals.push();
-        Proposal storage newProposal = proposals[proposals.length - 1];
-        newProposal.recipient = recipient;
-        newProposal.amount = amount;
-        newProposal.votes = 0;
-        newProposal.endTime = block.timestamp + minVoteDuration;
-        newProposal.unlockTime = block.timestamp + 48 hours; // Funds locked for 48 hours
-        newProposal.executed = false;
-
-        emit ProposalCreated(proposals.length - 1, recipient, amount);
-    }
-
-    function vote(uint proposalId) public onlyMember {
-        Proposal storage proposal = proposals[proposalId];
-        require(!proposal.voted[msg.sender], "Already voted");
-        require(block.timestamp < proposal.endTime, "Voting period ended");
-
-        uint256 voterBalance = governanceToken.balanceOf(msg.sender);
-        require(voterBalance > 0, "Must hold governance tokens to vote");
-
-        proposal.votes += voterBalance;
-        proposal.voted[msg.sender] = true;
-
-        emit Voted(proposalId, msg.sender, voterBalance);
-    }
-
-    function executeProposal(uint proposalId) public {
-        Proposal storage proposal = proposals[proposalId];
-        require(proposal.votes >= 500 * 10**18, "Not enough votes (500 tokens required)");
-        require(!proposal.executed, "Already executed");
-        require(block.timestamp >= proposal.unlockTime, "Funds still locked");
-
-        proposal.executed = true;
-        proposal.recipient.transfer(proposal.amount);
-
-        emit ProposalExecuted(proposalId);
-    }
-
-    function getETHPrice() public view returns (uint256) {
-        (, int price, , , ) = priceFeed.latestRoundData();
-        return uint(price) / 1e8;
-    }
-
-    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /* performData */) {
-        for (uint i = 0; i < proposals.length; i++) {
-            if (!proposals[i].executed && proposals[i].votes >= 500 * 10**18 && block.timestamp >= proposals[i].unlockTime) {
-                return (true, abi.encode(i));
-            }
-        }
-        return (false, "");
-    }
-
-    function performUpkeep(bytes calldata performData) external override {
-        uint proposalId = abi.decode(performData, (uint));
-        executeProposal(proposalId);
-    }
-
-    function depositToAave(uint256 amount) external onlyOwner {
-        lendingPool.deposit(address(this), amount, address(this), 0);
-    }
-
-    function withdrawFromAave(uint256 amount) external onlyOwner {
-        lendingPool.withdraw(address(this), amount, address(this));
-    }
-
-    function deposit() public payable {}
-
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
-    }
+interface IAaveLendingPool {
+   function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+   function withdraw(address asset, uint256 amount, address to) external;
 }
 
 
+contract TreasuryDAO is Ownable, AutomationCompatible {
+   struct Proposal {
+       address payable recipient;
+       uint256 amount;
+       uint256 votes;
+       uint256 endTime;
+       bool executed;
+       uint256 unlockTime;
+       mapping(address => bool) voted;
+   }
+
+
+   mapping(address => bool) public members;
+   Proposal[] public proposals;
+   IERC20 public governanceToken;
+   AggregatorV3Interface internal priceFeed;
+   IAaveLendingPool public lendingPool;
+   uint256 public minVoteDuration = 1 days;
+
+
+   event ProposalCreated(uint proposalId, address recipient, uint256 amount);
+   event Voted(uint proposalId, address voter, uint256 weight);
+   event ProposalExecuted(uint proposalId);
+   event FundsReleased(uint proposalId);
+
+
+   constructor(address tokenAddress, address priceFeedAddress, address lendingPoolAddress, address initialOwner)
+       Ownable(initialOwner)
+   {
+       governanceToken = IERC20(tokenAddress);
+       priceFeed = AggregatorV3Interface(priceFeedAddress);
+       lendingPool = IAaveLendingPool(lendingPoolAddress);
+       members[initialOwner] = true;
+   }
+
+
+   modifier onlyMember() {
+       require(members[msg.sender], "Not a DAO member");
+       _;
+   }
+
+
+   function addMember(address member) public onlyOwner {
+       members[member] = true;
+   }
+
+
+   function proposeFunding(address payable recipient, uint256 amount) public onlyMember {
+       require(address(this).balance >= amount, "Insufficient treasury balance");
+       proposals.push();
+       Proposal storage newProposal = proposals[proposals.length - 1];
+       newProposal.recipient = recipient;
+       newProposal.amount = amount;
+       newProposal.votes = 0;
+       newProposal.endTime = block.timestamp + minVoteDuration;
+       newProposal.executed = false;
+
+
+       emit ProposalCreated(proposals.length - 1, recipient, amount);
+   }
+
+
+   function vote(uint proposalId) public onlyMember {
+       Proposal storage proposal = proposals[proposalId];
+       require(!proposal.voted[msg.sender], "Already voted");
+       require(block.timestamp < proposal.endTime, "Voting period ended");
+
+
+       uint256 voterBalance = governanceToken.balanceOf(msg.sender);
+       require(voterBalance > 0, "Must hold governance tokens to vote");
+
+
+       proposal.votes += voterBalance;
+       proposal.voted[msg.sender] = true;
+
+
+       emit Voted(proposalId, msg.sender, voterBalance);
+   }
+
+
+   function executeProposal(uint proposalId) public {
+       Proposal storage proposal = proposals[proposalId];
+       require(proposal.votes >= 500 * 10**18, "Not enough votes (500 tokens required)");
+       require(!proposal.executed, "Already executed");
+       require(block.timestamp >= proposal.endTime, "Voting period not ended");
+       require(getETHPrice() > 1500, "ETH price too low");
+
+
+       proposal.executed = true;
+       proposal.unlockTime = block.timestamp + 48 hours;
+
+
+       emit ProposalExecuted(proposalId);
+   }
+
+
+   function releaseFunds(uint proposalId) public {
+       Proposal storage proposal = proposals[proposalId];
+       require(block.timestamp >= proposal.unlockTime, "Funds locked");
+       proposal.recipient.transfer(proposal.amount);
+
+
+       emit FundsReleased(proposalId);
+   }
+
+
+   function getETHPrice() public view returns (uint256) {
+       (, int price, , , ) = priceFeed.latestRoundData();
+       return uint256(price) / 1e8; // Convert price to USD
+   }
+
+
+   function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory performData) {
+       for (uint i = 0; i < proposals.length; i++) {
+           if (!proposals[i].executed && proposals[i].votes >= 500 * 10**18 && block.timestamp >= proposals[i].endTime) {
+               return (true, abi.encode(i));
+           }
+       }
+       return (false, "");
+   }
+
+
+   function performUpkeep(bytes calldata performData) external override {
+       uint proposalId = abi.decode(performData, (uint));
+       executeProposal(proposalId);
+   }
+
+
+   function depositToAave(uint256 amount) external onlyOwner {
+       lendingPool.deposit(address(this), amount, address(this), 0);
+   }
+
+
+   function withdrawFromAave(uint256 amount) external onlyOwner {
+       lendingPool.withdraw(address(this), amount, address(this));
+   }
+
+
+   function deposit() public payable {}
+
+
+   function getBalance() public view returns (uint256) {
+       return address(this).balance;
+   }
+}
